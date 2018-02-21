d8ac06768cb869289160de886bc302400f31075a
Martin Hochel <hochelmartin@gmail.com>
2018-02-22 00:04:46 +0100

    feat(storybook): add initial demo stories

---
+++ src/stories/index.ts
@@ -0,0 +1,38 @@
+import { storiesOf } from '@storybook/angular';
+import { Welcome, Button } from '@storybook/angular/demo';
+import { moduleMetadata } from './helpers';
+
+import { action } from '@storybook/addon-actions';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/angular';
+
+storiesOf('Welcome', module).add('to Storybook', () => ({
+  template: `<storybook-welcome-component></storybook-welcome-component>`,
+  props: {},
+  moduleMetadata: {
+    declarations: [Welcome],
+  },
+}));
+
+storiesOf('Button', module)
+  .addDecorator(
+    moduleMetadata({
+      declarations: [Button],
+    })
+  )
+  .add('with text', () => ({
+    template: `<storybook-button-component [text]="text" (onClick)="onClick($event)"></storybook-button-component>`,
+    props: {
+      text: 'Hello Button',
+      onClick: (event: MouseEvent) => {
+        console.log('some bindings work');
+        console.log(event);
+      },
+    },
+  }))
+  .add('with some emoji', () => ({
+    template: `<storybook-button-component [text]="text" (onClick)="onClick($event)"></storybook-button-component>`,
+    props: {
+      text: '😀 😎 👍 💯',
+      onClick: () => {},
+    },
+  }));
